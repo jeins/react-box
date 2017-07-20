b345f65f486ef5075b79672e1c70e32c86b7b848
jeins <pinghead123>
2017-07-20 18:27:10 +0200

    added new table style

--- src/app/components/UserList.js
+++ src/app/components/UserList.js
@@ -6,15 +6,10 @@ import Paper from 'material-ui/Paper';
 import RaisedButton from 'material-ui/RaisedButton';
 import FontIcon from 'material-ui/FontIcon';
 import {GridList} from 'material-ui/GridList';
-import {
-    Table,
-    TableBody,
-    TableHeader,
-    TableHeaderColumn,
-    TableRow,
-    TableRowColumn,
-} from 'material-ui/Table';
+import DataTables from 'material-ui-datatables';
 import UserDetail from './UserDetail';
+import {sortBy} from 'lodash';
+import {toJS} from 'mobx';
 
 const styles = {
     div: {
@@ -48,6 +43,80 @@ export default class UserList extends Component {
         this.user = this.props.user;
     }
 
+    setupTableHeader() {
+        return [
+            {
+                key: 'ewd_number',
+                label: 'Number',
+                sortable: true
+            },
+            {
+                key: 'ewd_tenant',
+                label: 'Tenant',
+                sortable: true
+            },
+            {
+                key: 'ewd_firstname',
+                label: 'Firstname',
+                sortable: true
+            },
+            {
+                key: 'ewd_lastname',
+                label: 'Lastname',
+                sortable: true
+            },
+            {
+                key: 'isActive',
+                label: 'Is Active?'
+            },
+            {
+                key: 'action',
+                label: ''
+            }
+        ];
+    }
+
+    setupTableContent() {
+        let table = [];
+        let action = {
+            view: (<IconButton iconClassName="material-icons" tooltip="View"
+                               tooltipPosition="top-right"
+                               onClick={() => this.user.viewUserData(index)}>pageview</IconButton>),
+            delete: (<IconButton iconClassName="material-icons" tooltip="Delete"
+                                 tooltipPosition="top-right"
+                                 onClick={() => this.user.remove(index)}>delete</IconButton>)
+        };
+
+        this.user.userList.forEach((user, index) => {
+            let conf = {};
+            conf['ewd_number'] = user.ewd_number;
+            conf['ewd_tenant'] = user.ewd_tenant;
+            conf['ewd_firstname'] = user.ewd_firstname;
+            conf['ewd_lastname'] = user.ewd_lastname;
+            conf['isActive'] = (
+                <Toggle
+                    id={index}
+                    onToggle={this._changeUserStatus}
+                    defaultToggled={user.isActive === 'true'}
+                />
+            );
+            conf['action'] = (
+                <div>
+                    <IconButton iconClassName="material-icons" tooltip="View"
+                                tooltipPosition="top-right"
+                                onClick={() => this.user.viewUserData(index)}>pageview</IconButton>
+                    <IconButton iconClassName="material-icons" tooltip="Delete"
+                                tooltipPosition="top-right"
+                                onClick={() => this.user.remove(index)}>delete</IconButton>
+                </div>
+            );
+
+            table.push(conf);
+        });
+
+        return table;
+    }
+
     componentWillMount() {
         this.props.user.getData();
     }
@@ -60,8 +129,17 @@ export default class UserList extends Component {
         this.user.save();
     };
 
+    handleFilterValueChange = (value) => {
+        console.log(value);
+    };
+
+    handleSortOrderChange = (key, order) => {
+        console.log(key + ' ' + order);
+    };
+
     render() {
         return (
+
             <div>
                 <GridList cols={2}>
                     <Paper zDepth={3} style={styles.div}>
@@ -70,44 +148,20 @@ export default class UserList extends Component {
                             icon={<FontIcon className="material-icons">add</FontIcon>}
                             onClick={() => this.user.cleanUp()}
                         />
-
-                        <Table fixedHeader={true}>
-                            <TableHeader displaySelectAll={false} adjustForCheckbox={false}>
-                                <TableRow style={{textAlign: 'center'}} colSpan="3">
-                                    <TableHeaderColumn>Number</TableHeaderColumn>
-                                    <TableHeaderColumn>Tenant</TableHeaderColumn>
-                                    <TableHeaderColumn>Firstname</TableHeaderColumn>
-                                    <TableHeaderColumn>Lastname</TableHeaderColumn>
-                                    <TableHeaderColumn>Status</TableHeaderColumn>
-                                    <TableHeaderColumn></TableHeaderColumn>
-                                </TableRow>
-                            </TableHeader>
-                            <TableBody displayRowCheckbox={false}>
-                                {this.user.userList.map((user, index) => (
-                                    <TableRow key={index}>
-                                        <TableRowColumn>{user.ewd_number}</TableRowColumn>
-                                        <TableRowColumn>{user.ewd_tenant}</TableRowColumn>
-                                        <TableRowColumn>{user.ewd_firstname}</TableRowColumn>
-                                        <TableRowColumn>{user.ewd_lastname}</TableRowColumn>
-                                        <TableRowColumn>
-                                            <Toggle
-                                                id={index}
-                                                onToggle={this._changeUserStatus}
-                                                defaultToggled={user.isActive === 'true'}
-                                            />
-                                        </TableRowColumn>
-                                        <TableRowColumn style={{overflow: 'visible'}}>
-                                            <IconButton iconClassName="material-icons" tooltip="View"
-                                                        tooltipPosition="top-right"
-                                                        onClick={() => this.user.viewUserData(index)}>pageview</IconButton>
-                                            <IconButton iconClassName="material-icons" tooltip="Delete"
-                                                        tooltipPosition="top-right"
-                                                        onClick={() => this.user.remove(index)}>delete</IconButton>
-                                        </TableRowColumn>
-                                    </TableRow>
-                                ))}
-                            </TableBody>
-                        </Table>
+                        <DataTables
+                            height={'auto'}
+                            selectable={false}
+                            showRowHover={true}
+                            columns={this.setupTableHeader()}
+                            data={this.setupTableContent()}
+                            showCheckboxes={false}
+                            showHeaderToolbar={true}
+                            onCellClick={this.handleCellClick}
+                            onCellDoubleClick={this.handleCellDoubleClick}
+                            onFilterValueChange={this.handleFilterValueChange}
+                            onSortOrderChange={this.handleSortOrderChange}
+                            count={1}
+                        />
                     </Paper>
 
                     <UserDetail/>
